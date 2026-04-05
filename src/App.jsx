619a2f433f22d6d6d13cc6e607d4de2e7401cbc4
Neeraj <jangidneeraj2007@gmail.com>
2026-04-05 18:35:16 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,126 @@
+
+import { Canvas } from '@react-three/fiber'
+import './App.css'
+import Dog from './com/Dog'
+
+function App() {
+
+  return (
+   <>
+   
+   <main>
+    <div className="images">
+          <img id='tomorrowland' src="/tommorowland.png" alt="" />
+          <img id='navy-pier' src="/navy-pier.png" alt="" />
+          <img id='msi-chicago' src="/msi-chicago.png" alt="" />
+          <img id='phone' src="/phone.png" alt="" />
+          <img id='kikk' src="/kikk.png" alt="" />
+          <img id='kennedy' src="/kennedy.png" alt="" />
+          <img id='opera' src="/opera.png" alt="" />
+        </div>
+    <Canvas style={{
+      height:"100vh",
+      width:"100vw",
+      position:"fixed",
+      top:"0",
+      left:"0",
+      zIndex:"1",
+      backgroundImage:"url(/public/col/background-l.png)",
+      backgroundRepeat:"no-repeat",
+      backgroundSize:"cover"
+    }}>
+    <Dog />
+    </Canvas>
+    <section id='section-1' >
+          <nav>
+            <div className="nav-elem">
+              <h3 style={{top:"-1"}}><img style={{height:"50PX",top:"0"}} src="/download.png"/></h3>
+              <h3>made by neeraj</h3>
+            </div>
+            <div className="nav-elem">
+              <i className="ri-arrow-right-s-line"></i> Our Show reel
+            </div>
+            <div className="nav-elem">
+              <i className="ri-menu-3-line"></i>
+            </div>
+          </nav>
+          <div className="middle">
+            <div className="left">
+              <h1>WE <br /> Make <br /> Good <br />Shit</h1>
+            </div>
+            <div className="right"></div>
+          </div>
+          <div className="bottom">
+            <div className="left"></div>
+            <div className="right">
+              <p>
+                Dogstudio is a multidisciplinary <br />
+                creative studio at the intersection <br />
+                of art, design and technology.
+              </p>
+            </div>
+          </div>
+
+          <div className="first-line"></div>
+          <div className="second-line"></div>
+
+        </section>
+    <section id='section-2' >
+          <div className="titles">
+            <div img-title="tomorrowland" className="title">
+              <small>2020 - ONGOING</small>
+              <h1>Tomorrowland</h1>
+            </div>
+            <div img-title="navy-pier" className="title">
+              <small>2020 - ONGOING</small>
+              <h1> Navy Pier</h1>
+            </div>
+            <div img-title="msi-chicago" className="title">
+              <small>2020 - ONGOING</small>
+              <h1>  MSI Chicago</h1>
+            </div>
+            <div img-title="phone" className="title">
+              <small>2020 - ONGOING</small>
+              <h1>This Was Louise’s Phone</h1>
+            </div>
+            <div img-title="kikk" className="title">
+              <small>2020 - ONGOING</small>
+              <h1>KIKK Festival 2018</h1>
+            </div>
+            <div img-title="kennedy" className="title">
+              <small>2020 - ONGOING</small>
+              <h1>The Kennedy Center</h1>
+            </div>
+            <div img-title="opera" className="title">
+              <small>2020 - ONGOING</small>
+              <h1>Royal Opera Of Wallonia</h1>
+            </div>
+          </div>
+
+
+        </section>
+    <section id='section-3'>
+      <div className='top'>
+        <div className='left'>
+          <h3>Lorem ipsum, dolor <be/> sit amet consectetur adipisicing elit.<br/> Laborum culpa minus soluta <br/>veniam hic quisquam?
+          </h3>
+        </div>
+        <div className='right'></div>
+      </div>
+      
+    </section>
+    <section id="section-4">
+      <div className='bottom'>
+        <div className="left"></div>
+        <div className="right">
+         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe vitae provident ipsa doloremque error, consequuntur voluptatem incidunt maxime earum veritatis nulla perspiciatis repellat totam at aperiam architecto consectetur odit velit. Molestiae veritatis iure eaque itaque atque corrupti fuga recusandae ducimus nostrum repellendus quisquam quod asperiores tempora inventore, quia tempore a.</p>
+         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe vitae provident ipsa doloremque error, consequuntur voluptatem incidunt maxime earum veritatis nulla perspiciatis repellat totam at aperiam architecto consectetur odit velit. Molestiae veritatis iure eaque itaque atque corrupti fuga recusandae ducimus nostrum repellendus quisquam quod asperiores tempora inventore, quia tempore a.</p>
+        </div>
+      </div>
+    </section>
+    </main>
+   </>
+  )
+}
+
+export default App
---
+++ src/com/Dog.jsx
@@ -0,0 +1,299 @@
+import React, { useRef } from 'react'
+import *as THREE from "THREE"
+import { OrbitControls, useGLTF, useTexture , useAnimations } from '@react-three/drei'
+import { Canvas, useThree } from '@react-three/fiber'
+import { color, normalMap, sample, texture, xor } from 'three/tsl'
+import { log } from 'THREE'
+import Animation from 'three/src/renderers/common/Animation.js'
+import { useEffect } from "react"
+import gsap from 'gsap'
+import { useGSAP } from '@gsap/react'
+import { ScrollTrigger } from 'gsap/ScrollTrigger'
+
+
+const Dog = () => { 
+
+    gsap.registerPlugin(useGSAP())
+    gsap.registerPlugin(ScrollTrigger)
+
+
+  const model = useGLTF("./dog.drc.glb")
+
+  useThree(({ camera, scene, gl }) => {
+    camera.position.z = 0.6
+    gl.toneMapping = THREE.ReinhardToneMapping
+    gl.outputColorSpace = THREE.SRGBColorSpace
+  })
+
+  const {actions} = useAnimations(model.animations, model.scene)
+     useEffect(()=>{
+      actions["Take 001"].play()
+     },[actions])
+
+  const [normalMap, sampleMatCap,] = (useTexture(["./dog_normals.jpg", "./col/mat-2.png",])).map(texture => {
+    texture.flipY = false
+    texture.colorSpace = THREE.SRGBColorSpace
+    return texture
+  })
+  const [branchmap,branchNormalmap] = (useTexture(["./col/branches_diffuse.jpeg","./col/branches_normals.jpeg"])).map(texture => {
+    texture.colorSpace = THREE.SRGBColorSpace
+    return texture
+  })
+
+  const [
+        mat1,
+        mat2,
+        mat3,
+        mat4,
+        mat5,
+        mat6,
+        mat7,
+        mat8,
+        mat9,
+        mat10,
+        mat11,
+        mat12,
+        mat13,
+        mat14,
+        mat15,
+        mat16,
+        mat17,
+        mat18,
+        mat19,
+        mat20
+    ] = (useTexture([
+        "/col/mat-1.png",
+        "/col/mat-2.png",
+        "/col/mat-3.png",
+        "/col/mat-4.png",
+        "/col/mat-5.png",
+        "/col/mat-6.png",
+        "/col/mat-7.png",
+        "/col/mat-8.png",
+        "/col/mat-9.png",
+        "/col/mat-10.png",
+        "/col/mat-11.png",
+        "/col/mat-12.png",
+        "/col/mat-13.png",
+        "/col/mat-14.png",
+        "/col/mat-15.png",
+        "/col/mat-16.png",
+        "/col/mat-17.png",
+        "/col/mat-18.png",
+        "/col/mat-19.png",
+        "/col/mat-20.png",
+    ])).map(texture => {
+        texture.colorSpace = THREE.SRGBColorSpace
+        return texture
+    })
+
+     const material = useRef({
+        uMatcap1: { value: mat19 },
+        uMatcap2: { value: mat2 },
+        uProgress: { value: 1.0 }
+    })
+
+
+  const dogmaterial = new THREE.MeshMatcapMaterial({
+        normalMap: normalMap,
+        matcap: sampleMatCap
+      })
+
+  const branchMaterial = new THREE.MeshMatcapMaterial({
+    normalMap: branchNormalmap,
+    map: branchmap
+  })
+
+
+ function onBeforeCompile(shader) {
+        shader.uniforms.uMatcapTexture1 = material.current.uMatcap1
+        shader.uniforms.uMatcapTexture2 = material.current.uMatcap2
+        shader.uniforms.uProgress = material.current.uProgress
+
+        // Store reference to shader uniforms for GSAP animation
+
+        shader.fragmentShader = shader.fragmentShader.replace(
+            "void main() {",
+            `
+        uniform sampler2D uMatcapTexture1;
+        uniform sampler2D uMatcapTexture2;
+        uniform float uProgress;
+
+        void main() {
+        `
+        )
+
+        shader.fragmentShader = shader.fragmentShader.replace(
+            "vec4 matcapColor = texture2D( matcap, uv );",
+            `
+          vec4 matcapColor1 = texture2D( uMatcapTexture1, uv );
+          vec4 matcapColor2 = texture2D( uMatcapTexture2, uv );
+          float transitionFactor  = 0.2;
+          
+          float progress = smoothstep(uProgress - transitionFactor,uProgress, (vViewPosition.x+vViewPosition.y)*0.5 + 0.5);
+
+          vec4 matcapColor = mix(matcapColor2, matcapColor1, progress );
+        `
+        )
+    }
+
+    dogmaterial.onBeforeCompile = onBeforeCompile
+  model.scene.traverse((child) => {
+    if (child.name.includes("DOG")) {
+      child.material = dogmaterial
+    }else 
+      child.material = branchMaterial
+  })
+
+  const dogmodel = useRef(model)
+
+
+  useGSAP(()=>{
+    const tl = gsap.timeline({
+      scrollTrigger:{
+        trigger:"#section-1",
+        endTrigger:"#section-4",
+        start:"top top",
+        end:"bottom bottom",
+        // markers:true,
+        scrub:true
+      }
+    })
+
+    tl.to(dogmodel.current.scene.position,{
+      z:"-=0.75 ",
+      y:"+=0.1"
+    })
+    .to(dogmodel.current.scene.rotation,{
+      x:`+=${Math.PI/15}`
+
+    })
+  .to(dogmodel.current.scene.rotation,{
+    y:`-=${Math.PI}`,
+   },"thir")
+   .to(dogmodel.current.scene.position,{
+    x:"-=0.5",
+    z:"+=0.6",
+    y:"-=0.1"
+   },"thir")
+    
+    
+  })
+   useEffect(() => {
+
+        document.querySelector(`.title[img-title="tomorrowland"]`).addEventListener("mouseenter", () => {
+            material.current.uMatcap1.value = mat19
+            gsap.to(material.current.uProgress, {
+                value: 0.0,
+                duration: 0.3,
+                onComplete: () => {
+                    material.current.uMatcap2.value = material.current.uMatcap1.value
+                    material.current.uProgress.value = 1.0
+                }
+            })
+        })
+        document.querySelector(`.title[img-title="navy-pier"]`).addEventListener("mouseenter", () => {
+
+            material.current.uMatcap1.value = mat8
+            
+            gsap.to(material.current.uProgress, {
+                value: 0.0,
+                duration: 0.3,
+                onComplete: () => {
+                    material.current.uMatcap2.value = material.current.uMatcap1.value
+                    material.current.uProgress.value = 1.0
+                }
+            })
+        })
+        document.querySelector(`.title[img-title="msi-chicago"]`).addEventListener("mouseenter", () => {
+
+            material.current.uMatcap1.value = mat9
+            
+            gsap.to(material.current.uProgress, {
+                value: 0.0,
+                duration: 0.3,
+                onComplete: () => {
+                    material.current.uMatcap2.value = material.current.uMatcap1.value
+                    material.current.uProgress.value = 1.0
+                }
+            })
+        })
+        document.querySelector(`.title[img-title="phone"]`).addEventListener("mouseenter", () => {
+
+            material.current.uMatcap1.value = mat12
+            
+            gsap.to(material.current.uProgress, {
+                value: 0.0,
+                duration: 0.3,
+                onComplete: () => {
+                    material.current.uMatcap2.value = material.current.uMatcap1.value
+                    material.current.uProgress.value = 1.0
+                }
+            })
+        })
+        document.querySelector(`.title[img-title="kikk"]`).addEventListener("mouseenter", () => {
+
+            material.current.uMatcap1.value = mat10
+            
+            gsap.to(material.current.uProgress, {
+                value: 0.0,
+                duration: 0.3,
+                onComplete: () => {
+                    material.current.uMatcap2.value = material.current.uMatcap1.value
+                    material.current.uProgress.value = 1.0
+                }
+            })
+        })
+        document.querySelector(`.title[img-title="kennedy"]`).addEventListener("mouseenter", () => {
+
+            material.current.uMatcap1.value = mat8
+            
+            gsap.to(material.current.uProgress, {
+                value: 0.0,
+                duration: 0.3,
+                onComplete: () => {
+                    material.current.uMatcap2.value = material.current.uMatcap1.value
+                    material.current.uProgress.value = 1.0
+                }
+            })
+        })
+        document.querySelector(`.title[img-title="opera"]`).addEventListener("mouseenter", () => {
+
+            material.current.uMatcap1.value = mat13
+            
+            gsap.to(material.current.uProgress, {
+                value: 0.0,
+                duration: 0.3,
+                onComplete: () => {
+                    material.current.uMatcap2.value = material.current.uMatcap1.value
+                    material.current.uProgress.value = 1.0
+                }
+            })
+        })
+        document.querySelector(`.titles`).addEventListener("mouseleave", () => {
+
+            material.current.uMatcap1.value = mat2
+            
+            gsap.to(material.current.uProgress, {
+                value: 0.0,
+                duration: 0.3,
+                onComplete: () => {
+                    material.current.uMatcap2.value = material.current.uMatcap1.value
+                    material.current.uProgress.value = 1.0
+                }
+            })
+        })
+
+    }, [])
+
+  return (
+    <>
+      <primitive object={model.scene} position={[0.2, -0.55, 0.1]} rotation={[0, Math.PI /6, 0]} />
+      <directionalLight position={[0, 5, 5]} color={0xffffff} intensity={10} />
+      {/* <OrbitControls /> */}
+
+    </>
+  )
+}
+
+export default Dog
